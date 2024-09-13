db1017207ac752bb11f0e5e5d9178d5c7fc5a4e4
Micaele25 <132933562+Micaele25@users.noreply.github.com>
2024-09-13 14:42:42 -0300

    Update Main.java

--- Veiculos/src/Main.java
+++ Veiculos/src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Veiculo[] veiculos = {
-                new Carro("Toyota", "Corolla", 2024, 4),
+                new Carro("Toyota", "Hilux", 2014, 2),
                 new Moto("Honda", "CB300", 2024, 300),
-                new Caminhao("Volvo", "FH", 2021, 20.5),
-                new Onibus("Mercedes", "O500", 2023, 45)
+                new Caminhao("Scania", "R 500", 2019, 20.5),
+                new Onibus("Mercedes", "OF 1721", 2021, 45)
         };
 
 
